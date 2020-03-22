91920a6b0d1718475522bfb9505642dc7b30d9bc
Brahialis0209 <rafantem@mail.ru>
2020-03-22 21:46:27 +0400

    fix mypy errors in pars_file.py

--- src/logic/pars_file.py
+++ src/logic/pars_file.py
@@ -3,11 +3,11 @@ from collections import defaultdict
 
 def pars_config_file(parameter_cmd: str):
     with open(parameter_cmd) as config_handler:
-        graph = defaultdict(list)
+        graph: defaultdict = defaultdict(list)
         start_node = config_handler.readline().split('=')[1].replace("\n", '')
         traversal = config_handler.readline().split('=')[1].replace("\n", '')
-        for item in config_handler:
-            item = item.split()
+        for line in config_handler:
+            item = line.split()
             from_node = item[0]
             to_node = item[1]
             if to_node == 'empty':
