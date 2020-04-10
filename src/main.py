3310e423d0717a1d59a6d0d9295688f443bbc3f9
Brahialis0209 <rafantem@mail.ru>
2020-04-11 00:20:10 +0400

    use argparse packet

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 sys.path.append('../')
 
 from src.alg.graph_traversal import traversal_graph
@@ -6,14 +7,10 @@ from src.alg.pars_file import pars_example_file
 
 
 def main():
-    parameters_cmd = list()
-    for param in sys.argv:
-        parameters_cmd.append(param)
-    if len(parameters_cmd) > 2:
-        print("example file error!")
-        return
-    parameter_cmd = parameters_cmd[1]
-    start_node, traversal, graph = pars_example_file(parameter_cmd)
+    parser = argparse.ArgumentParser(description='shading graphs')
+    parser.add_argument('example', type=str, help='Input file name with example.')
+    args = parser.parse_args()
+    start_node, traversal, graph = pars_example_file(args.example)
     traversal_graph(graph, start_node, traversal, True)
 
 
